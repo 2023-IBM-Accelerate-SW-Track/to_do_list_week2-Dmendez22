99429e12055d8abdfea48e3de591236c74b91454
Danny Mendez <dmendezvm@gmail.com>
2023-06-16 17:17:33 -0700

    Completed step 3 which adds delete feature

--- src/component/todos.js
+++ src/component/todos.js
@@ -9,7 +9,7 @@ import { Card, CardContent, Grid, ListItemText, ListItemButton, Checkbox } from 
 // 3. The map function is called to assign each array item with a key
 // 4. Think of lines 14-23 as a loop. For each todo in the todo list, we want to give the list item
 // a key, and it's own card shown in the UI
-const Todos = ({ todos }) => {
+const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
       return (
@@ -17,7 +17,7 @@ const Todos = ({ todos }) => {
           <Card style={{ marginTop: 10 }}>
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} */}
-            <Checkbox style={{ paddingLeft: 0 }} color="primary" />
+            <Checkbox style={{ paddingLeft: 0 }} color="primary" onClick={() => deleteTodo(todo.id)} />
             <ListItemButton component="a" href="#simple-list">
               <ListItemText primary={todo.content} secondary={todo.date} />
             </ListItemButton>
